5346ec7f70abfc3851264735d011c1a5383bef87
@vitaliylukin91 <vitaliylukin91@gmail.com>
2020-06-24 19:01:28 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 jQuery(function($) {
     $(document).ready(function(){
-
+        const scene = document.getElementById('scene');
+        const parallaxInstance = new Parallax(scene);
     });
 });
 
